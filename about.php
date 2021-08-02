13ab480eca7cda8fa3da5ce3b6ca12a29c0e3826
Dawn Andario <57174907+dawnmarieee@users.noreply.github.com>
2021-08-02 21:27:37 +0800

    Add files via upload
    Haven't included the booking application. Files are bigger than expected.

---
+++ about.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+        <title>Welcome to AC-DC Service Center</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link href="./css/style7.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="./css/mark-your-calendar.css" />
+    </head>
+    <body>
+        
+        <div class="grid">
+            <section id="floating"></section>
+            <header>
+                <nav>
+                    <div class="dropdown">
+                        <button class="dropbtn" id="hidebtn"><span class="fa fa-bars fa-2x" aria-hidden="true"></span></button>
+                            <div class="dropdown-content">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="explore.php">Explore</a></li>
+                                <li><a class="active" href="#">About</a></li>
+                                <li><a href="booking/home.php" target="_blank">Sign In</a></li>
+                            </ul>
+                            </div>
+                    </div>
+                </nav>
+                <h2><span style="font-weight: 900; font-style: italic; color: rgb(6,16,88);">AC-DC Service Center</h2></span>
+                <main class="contents box">
+                    <div class="contact-box">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <ul>
+                    <li><h4>Contact us</h4></li>
+                    <li>
+                        <div class="icon">
+                            
+                        </div>
+                        <div class="links phone">
+                        <i class="fa fa-phone" aria-hidden="true"></i>&nbsp; <a class="phone-number" href="#">+09294654668</a>
+                        </div><br>
+                    </li>
+                        <li>
+                            <div class="icon">
+                                
+                            </div>
+                            <div class="links emails">
+                            <i class="fa fa-at" aria-hidden="true"></i>&nbsp;<a href="mailto:demo@acdcservice.me">demo@acdcservice.me</a>
+                            </div><br>
+                        </li>
+                    <li>
+                        <div class="icon">
+                            
+                        </div>
+                        <div class="links">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;<a href="#" target="_blank">Lambunao, Iloilo</a>
+                        </div><br>
+                    </li>
+            </ul>
+                    </div>
+                <div class="lorem">
+                    
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+                </div>
+                </main> 
+                
+                <footer>
+                    <p>️&copy; West Visayas State University - Lambunao Campus 2020</p>
+                </footer>
+                
+            </header>
+        </div>  
+            <!--jquery-->
+            <script type="text/javascript" src="js/jquery.js"></script>
+            <script type="text/javascript" src="jqueryui/jquery-ui.js"></script>
+            <script>
+                const faBar {
+
+                }
+            </script>
+    </body>
+</html>
---
+++ explore.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+        <title>Welcome to AC-DC Service Center</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link href="./css/style7.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="./css/mark-your-calendar.css" />
+    </head>
+    <body>
+        
+        <div class="grid">
+            <section id="floating"></section>
+            <header>
+                <nav>
+                    <div class="dropdown">
+                        <button class="dropbtn" id="hidebtn"><span class="fa fa-bars fa-2x" aria-hidden="true"></span></button>
+                            <div class="dropdown-content">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a class="active" href="#">Explore</a></li>
+                                <li><a href="about.php">About</a></li>
+                                <li><a href="booking/home.php" target="_blank">Sign In</a></li>
+                            </ul>
+                            </div>
+                    </div>
+                </nav>
+                <h2><span style="font-weight: 900; font-style: italic; color: rgb(6,16,88);">AC-DC Service Center</h2></span>
+                <main class="contents box">
+                    <div class="contact-box">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <h4>Opening Hours</h4>
+                        <ul> 
+                            <li>Mon	08:00 - 18:00</li><br>
+                            <li>Tue	08:00 - 18:00</li><br>
+                            <li>Wed	08:00 - 18:00</li><br>
+                            <li>Thu	08:00 - 18:00</li><br>
+                            <li>Fri	08:00 - 18:00</li><br>
+                            <li>Sat	08:00 - 12.00</li><br>
+                            <li>Sun	Closed</li> 
+                        </ul>
+                    </div>
+                <div class="lorem">
+                    
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+                </div>
+                </main> 
+                
+                <footer>
+                    <p>️&copy; West Visayas State University - Lambunao Campus 2020</p>
+                </footer>
+                
+            </header>
+        </div>  
+            <!--jquery-->
+            <script type="text/javascript" src="js/jquery.js"></script>
+            <script type="text/javascript" src="jqueryui/jquery-ui.js"></script>
+            <script>
+                const faBar {
+
+                }
+            </script>
+    </body>
+</html>
