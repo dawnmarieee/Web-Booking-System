bea064e1d7941ec41dbf2b55b3dc53c18db1114d
Dawn Andario <57174907+dawnmarieee@users.noreply.github.com>
2021-08-11 00:42:17 +0800

    Update index.php
    Changed line 69 <h2><span> tags

--- index.php
+++ index.php
@@ -32,7 +32,71 @@
                             </div>
                     </div>
                 </nav>
-                <h2><span style="font-weight: 900; font-style: italic; color: rgb(6,16,88);">AC-DC Service Center</h2></span>
+                
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="cache-control" content="no-cache">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+        <title>Welcome to AC-DC Service Center</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link href="./css/style6.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="./css/mark-your-calendar.css" />
+    </head>
+    <body>
+        
+        <div class="grid">
+            <section id="floating"></section>
+            <header>
+                <nav>
+                    <div class="dropdown">
+                        <button class="dropbtn" id="hidebtn"><span class="fa fa-bars fa-2x" aria-hidden="true"></span></button>
+                            <div class="dropdown-content">
+                            <ul>
+                                <li><a class="active" href="#">Home</a></li>
+                                <li><a href="explore.php">Explore</a></li>
+                                <li><a href="about.php">About</a></li>
+                                <li><a href="booking/home.php" target="_blank">Sign In</a></li>
+                            </ul>
+                            </div>
+                    </div>
+                </nav>
+                <h2><span style="font-weight: 900; font-style: italic; color: rgb(6,16,88);">AC-DC Service Center</span></h2>
+                <main class="contents box">
+                <div id="info1">
+                    <img class="graphic3" src="./images/tv.png">
+                    <img class="graphic2" src="./images/ref.png">
+                    <img class="graphic1" src="./images/tv.png">
+                    <img class="graphic0" src="./images/aircon.png">
+                </div>
+                <div class="info2">
+                        <h1>Aircon installation, maintenance & home repair services</h1>
+                        <p>Offers aircon general cleaning, repair, dismantling for window types and split types.</p><br>
+                        <a href="booking/home.php" data-toggle="modal"><button class="button1" type="button">Book a Service</button><br></a> 
+                </div>
+                </main> 
+                
+                <footer>
+                    <p>️&copy; West Visayas State University - Lambunao Campus 2020</p>
+                </footer>
+                
+            </header>
+        </div>  
+            <!--jquery-->
+            <script type="text/javascript" src="js/jquery.js"></script>
+            <script type="text/javascript" src="jqueryui/jquery-ui.js"></script>
+            <script>
+                const faBar {
+
+                }
+            </script>
+    </body>
+</html>
                 <main class="contents box">
                 <div id="info1">
                     <img class="graphic3" src="./images/tv.png">
