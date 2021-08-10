fe9adbb0d4addce980c9095a7826a1adce2d2769
Dawn Andario <57174907+dawnmarieee@users.noreply.github.com>
2021-08-11 00:56:32 +0800

    Update about.php

--- about.php
+++ about.php
@@ -36,38 +36,38 @@
                     <div class="contact-box">
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <ul>
-                    <li><h4>Contact us</h4></li>
-                    <li>
-                        <div class="icon">
+                            <li><h4>Contact us</h4></li>
+                            <li>
+                                <div class="icon">
                             
-                        </div>
-                        <div class="links phone">
-                        <i class="fa fa-phone" aria-hidden="true"></i>&nbsp; <a class="phone-number" href="#">+09294654668</a>
-                        </div><br>
-                    </li>
-                        <li>
-                            <div class="icon">
+                                </div>
+                                <div class="links phone">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>&nbsp; <a class="phone-number" href="#">+09294654668</a>
+                                 </div><br>
+                            </li>
+                            <li>
+                                    <div class="icon">
                                 
-                            </div>
-                            <div class="links emails">
-                            <i class="fa fa-at" aria-hidden="true"></i>&nbsp;<a href="mailto:demo@acdcservice.me">demo@acdcservice.me</a>
-                            </div><br>
-                        </li>
-                    <li>
-                        <div class="icon">
+                                    </div>
+                                    <div class="links emails">
+                                        <i class="fa fa-at" aria-hidden="true"></i>&nbsp;<a href="mailto:demo@acdcservice.me">demo@acdcservice.me</a>
+                                    </div><br>
+                            </li>
+                            <li>
+                                <div class="icon">
                             
-                        </div>
-                        <div class="links">
-                        <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;<a href="#" target="_blank">Lambunao, Iloilo</a>
-                        </div><br>
-                    </li>
-            </ul>
+                                </div>
+                                <div class="links">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;<a href="#" target="_blank">Lambunao, Iloilo</a>
+                                </div><br>
+                            </li>
+                        </ul>
                     </div>
-                <div class="lorem">
+                    <div class="lorem">
                     
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
-                </div>
+                    </div>
                 </main> 
                 
                 <footer>
